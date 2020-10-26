de28ee909a7ad767f086d9808d319646e63acf95
Steven Mann <stevencmann2@gmail.com>
2020-10-26 17:39:16 -0500

    changed accordian to bootstrap syntax; scrolling header change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,65 @@
 $(document).ready(function(){
 
+
+    //navbar change on scroll
+    $(window).scroll(function() {
+        $(".navbar").toggleClass('scrolled', $(this).scrollTop() > $(".navbar").height()*5)
+    })
+
+
+
+
+  
     // Paragraphs
     const itParagraph = $("#it-content")
     const energyParagraph = $("#energy-content")
+
     // clickable icons
     const itToggle = $("#it-toggle")
     const energyToggle = $("#energy-toggle")
     const itToggleUp = $('#it-toggle-up').hide()
     const energyToggleUp = $('#energy-toggle-up').hide()
+    
+    //ENERGY CONTENT
+    energyToggle.on( "click", function(e) {
+        e.preventDefault();
+        energyParagraph.collapse('toggle')
+        energyToggle.hide()
+        energyToggleUp.show()
 
-    $("#it-content ").hide();
-    $("#energy-content").hide();
-    $('#energy-toggle-up').hide()
-    $('#it-toggle-up').hide()
+    });
 
-    $( "#it-toggle").on( "click", function( event ) {
-        itParagraph.show();
+    energyToggleUp.on( "click", function(e) {
+        e.preventDefault();
+        energyParagraph.collapse('toggle')
+        energyToggle.show()
+        energyToggleUp.hide()
+
+    });
+
+    //IT CONTENT
+    itToggle.on( "click", function(e) {
+        e.preventDefault();
+        itParagraph.collapse('toggle')
         itToggle.hide()
         itToggleUp.show()
-      });
 
-      $( "#energy-toggle" ).on( "click", function( event ) {
-        energyParagraph.show();
-        energyToggle.hide();
-        energyToggleUp.show();
-      });
+    });
 
-      itToggleUp.on( "click", function( event ) {
-        itParagraph.hide();
+    itToggleUp.on( "click", function(e) {
+        e.preventDefault();
+        itParagraph.collapse('toggle')
         itToggle.show()
-        itToggleUp.hide()
-      });
+       itToggleUp.hide()
 
-      energyToggleUp.on( "click", function( event ) {
-        energyParagraph.hide();
-        energyToggle.show();
-        energyToggleUp.hide();
-      });
+    });
 
 
 
 
+
+
+
+    
 
 });
