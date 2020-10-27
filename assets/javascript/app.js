733b22cdce55a11823064c184fdafa0ff350f1a3
Steven Mann <stevencmann2@gmail.com>
2020-10-27 17:03:26 -0500

    changing section order, adding photos and js for accordian

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,9 +37,9 @@ $(document).ready(function(){
     //navbar change on scroll
     $(window).scroll(function() {
         // toggle navbar color 
-        $(".navbar").toggleClass('scrolled', $(this).scrollTop() > $(".navbar").height()*5);
+        $(".navbar").toggleClass('scrolled', $(this).scrollTop() > $(".navbar").height()*1.5);
         //toggle logo disapear
-       $("#initial-logo").toggleClass("animate__animated animate__fadeOut", $(this).scrollTop() > $(".navbar").height()*5)
+    //    $("#initial-logo").toggleClass("animate__animated animate__fadeOut", $(this).scrollTop() > $(".navbar").height()*1.5)
     })
 
 
@@ -89,6 +89,88 @@ $(document).ready(function(){
        itToggleUp.hide()
 
     });
+
+    /////// Banner accordian
+
+    const it = $("#it");
+    const energy = $("#energy");
+    const partnerships = $("#partnernships")
+    const content = $("#accordian-content")
+
+    it.on( "click", function(e) {
+       
+        e.preventDefault();
+        content.empty();
+        content.addClass("text-center")
+        content.append(
+            `
+            <div class="row">
+                <div class="col">
+                    <h3>Cloud Computing</h3>
+                    <p>On the cutting edge of technology, Lone Star College is proud to offer our new IT program.
+                        The IT program is a culmination of our long term promise to deliver accesible and affordable cloud
+                        computing to the Houston area.</p>
+                </div>
+
+                <div class="col h-10">
+                    <img src="./assets/images/cloud-computing.jpg" class="img-fluid" alt="Cloud Computing">
+                </div>
+
+            </div>
+            `
+        )
+        content.collapse('toggle');
+       
+    })
+
+    energy.on( "click", function(e) {
+        e.preventDefault();
+        content.empty();
+        content.addClass("text-center")
+        content.append(
+            `<div class="row justify-content-center" style="background-color: grey">
+
+                <div class="col-lg-6 md-6 sm-12">
+                    <h3>An immersive experience, unlike any other.</h3>
+                    <p>Our cutting edge cirriculum in green energy techonologies boasts the first ever co-operative program,
+                        designed in part by our airship crew. Get hands on exprience working with mechanics and electrical systems that keep the airship afloat. 
+                    </p>
+                </div>
+
+                <div class="col-lg-6 md-6 sm-12">
+                    <img src="./assets/images/solar-panels.jpg" class="img-fluid" alt="Solar Panels">
+                </div>
+
+            </div>
+            `
+        )
+
+        content.collapse('toggle');
+        
+       
+    })
+
+    partnerships.on( "click", function(e) {
+        e.preventDefault();
+        content.empty();
+        content.addClass("text-center")
+        content.append(
+            `<div>
+                <br>
+                <br>
+                &#8226; Fuel Cell Systems Electrician Marketable Skills Award
+                <br>
+                &#8226; Solar Photovoltaic System Electrician Marketable Skills Award
+                <br>
+                &#8226; Wind Turbine System Electrician Marketable Skills Award
+            </div>
+            `
+        )
+        content.collapse('toggle');
+        
+        console.log('click partnerships')
+    })
+
 
 
 
